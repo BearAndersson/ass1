7b18f6ec1e7b746594299cfee7eebf335dc8695f
Björn Andersson <bjornandersson@Bjorns-MBP.lan>
2020-01-28 21:16:59 +0100

    Updates

---
+++ precache-manifest.f328defe2e029db69ced8d7c973c3fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607b09094bd9da6d7ead041b06d92896",
+    "url": "/ass1/index.html"
+  },
+  {
+    "revision": "110b403d31a32d1fba3f",
+    "url": "/ass1/static/css/main.df87dc81.chunk.css"
+  },
+  {
+    "revision": "26a15461257fb22b1f2d",
+    "url": "/ass1/static/js/2.d2fb574e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ass1/static/js/2.d2fb574e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "110b403d31a32d1fba3f",
+    "url": "/ass1/static/js/main.140e6f27.chunk.js"
+  },
+  {
+    "revision": "116fe90516b3f1e5f877",
+    "url": "/ass1/static/js/runtime-main.7d5182d5.js"
+  }
+]);
